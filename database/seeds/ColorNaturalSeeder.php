6e25666663f9d316a22ac5427f5224a4ea72c0b3
Tran Tuan <tran.van.tuan@mqsolutions.vn>
2019-01-07 13:10:39 +0700

    update crawler

---
+++ database/seeds/ColorNaturalSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Color;
+use App\Models\ColorGroup;
+class ColorNaturalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $colors = [];
+
+        $colorGrp = ColorGroup::where('name', 'Họ màu thiên nhiên')->first();
+        if ($colorGrp) {
+            foreach($colors as $cl) {
+                $color = Color::create([
+                    'name' => $cl['name'],
+                    'color' => $cl['color'],
+                    'mixed_by_computer' => $cl['mixed_by_computer'],
+                    'is_deep_color' => $cl['is_deep_color'],
+                    'is_popular' => $cl['is_popular'],
+                    'color_group_id' => $colorGrp->id
+                ]);
+                if ($color) {
+                    $color->surfaces()->sync($cl['surfaces']);
+                    $color->projecttypes()->sync($cl['projectTypes']);
+                }
+            }
+        }
+    }
+}
